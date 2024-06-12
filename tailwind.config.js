b0d234e190281d139345643761748378b0eac6e3
Binh Q. Ung <binhuq@gmail.com>
2024-06-12 17:01:27 +0700

    update template

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      aspectRatio: {
+        "3/2": "3 / 2",
+      },
+    },
+  },
+  corePlugins: {
+    // aspectRatio: false,
+  },
+  plugins: [
+    // require("@tailwindcss/aspect-ratio"),
+  ],
+};
